5b9e6a5fcc075cc75b6e65a22b293fa392f317dc
Mossroy <mossroy@mossroy.fr>
2017-06-29 17:42:33 +0200

    Test on Firefox 47

--- nightwatch.js
+++ nightwatch.js
@@ -27,7 +27,7 @@ module.exports = {
     "firefox" : {
       "desiredCapabilities": {
         "browserName": "firefox",
-        "version": "49.0",
+        "version": "47.0",
         "javascriptEnabled": true,
         "acceptSslCerts": true,
         "build": "build-" + TRAVIS_JOB_NUMBER,
